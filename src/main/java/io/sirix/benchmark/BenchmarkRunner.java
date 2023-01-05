329eb5b5c361f75b8c1adfcfded8e7403bfa3fe6
johannesl <lichtenberger.johannes@gmail.com>
2023-01-05 14:09:33 +0100

    Initial commit

---
+++ src/main/java/io/sirix/benchmark/BenchmarkRunner.java
@@ -0,0 +1,28 @@
+package io.sirix.benchmark;
+
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.io.IOException;
+
+public class BenchmarkRunner {
+  public static void main(String[] args) throws RunnerException, IOException {
+    final Options opt = new OptionsBuilder().jvmArgsAppend("-ea",
+                                                           "--enable-preview",
+                                                           "--add-exports=java.base/jdk.internal.ref=ALL-UNNAMED",
+                                                           "--add-exports=java.base/sun.nio.ch=ALL-UNNAMED",
+                                                           "--add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED",
+                                                           "--add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
+                                                           "--add-opens=jdk.compiler/com.sun.tools.javac=ALL-UNNAMED",
+                                                           "--add-opens=java.base/java.lang=ALL-UNNAMED",
+                                                           "--add-opens=java.base/java.lang.reflect=ALL-UNNAMED",
+                                                           "--add-opens=java.base/java.io=ALL-UNNAMED",
+                                                           "--add-opens=java.base/java.util=ALL-UNNAMED")
+                                            .include(".*XMarkBench.*")
+                                            .forks(1)
+                                            .build();
+    new Runner(opt).run();
+  }
+}
